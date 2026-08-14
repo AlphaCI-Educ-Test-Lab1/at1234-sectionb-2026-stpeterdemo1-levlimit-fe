9148a888bff517744abb927905e2b6139a6bf2bf
LloydL1m <limlloyd1014@gmail.com>
2026-08-14 10:47:30 +0800

    chore: add src/index.spec.ts (AlphaCI CI/CD scaffold)

---
+++ src/index.spec.ts
@@ -0,0 +1,7 @@
+import { SERVICE_NAME } from './index';
+
+describe('at1234-sectionb-2026-stpeterdemo1-levlimit-fe', () => {
+  it('should export SERVICE_NAME', () => {
+    expect(SERVICE_NAME).toBe('at1234-sectionb-2026-stpeterdemo1-levlimit-fe');
+  });
+});
