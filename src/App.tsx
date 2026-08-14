e585a7e0f96a47eb0c8c3cb9dcde000bf41adf52
LloydL1m <limlloyd1014@gmail.com>
2026-08-14 10:47:36 +0800

    chore: add src/App.tsx (AlphaCI CI/CD scaffold)

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { TemplatePanel } from './components/TemplatePanel';
+
+export function App() {
+  return (
+    <main>
+      <h1>at1234-sectionb-2026-stpeterdemo1-levlimit-fe</h1>
+      <TemplatePanel />
+    </main>
+  );
+}
